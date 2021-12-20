34a3829143349e9a4f6c9dada5798a32182dad39
Edmund Mills <edmund.martin.mills@gmail.com>
2021-12-20 12:56:50 -0700

    Add random essay and random span methods to Dataset

--- dataset.py
+++ dataset.py
@@ -1,24 +1,62 @@
+import random
 from pathlib import Path
+from typing import List
 
 import pandas as pd
 
 class ArgumentDataset:
-    def __init__(self):
+    def __init__(self) -> None:
         self.data_path = Path('data')
+        self.essay_dir = self.data_path / 'train'
         self.df = pd.read_csv(self.data_path / 'train.csv')
 
     def __len__(self):
         return len(self.df)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> pd.DataFrame:
         return self.df.iloc[idx]
 
-    def essays(self):
+    def essay_paths(self) -> List[Path]:
+        return list(self.essay_dir.iterdir())
+
+    def essays(self, number=None):
         idx = 0
-        while idx < len(self):
+        count = 0
+        number = number or float('Inf')
+        print(number)
+        while idx < len(self) and count < number:
+            count += 1
+            print(count)
             init_idx = idx
-            essay_number = self[idx].loc['id']
-            while idx < len(self) and self[idx].loc['id'] == essay_number:
+            essay_id = self[idx].loc['id']
+            while idx < len(self) and self[idx].loc['id'] == essay_id:
                 idx += 1
             yield self[init_idx:idx]
+
+    def arguments(self, number=None):
+        number = number or float('Inf')
+        return self[:number].iterrows()
+
+    def random_essay(self, num_essays=1) -> List[str]:
+        essays = []
+        for essay_path in random.sample(self.essay_paths(), num_essays):
+            with open(essay_path) as f:
+                essay = f.read()
+                essays.append(essay)
+        return essays
+
+    def random_span(self, num_words, num_spans=1):
+        spans = []
+        for essay in self.random_essay(num_spans):
+            words = essay.split()
+            start_idx = len(words) - num_words
+            if start_idx < 0:
+                spans.append(essay)
+            else:
+                start_idx = random.sample(range(start_idx), 1)[0]
+                stop_idx = start_idx + num_words
+                words = words[start_idx:stop_idx]
+                span = ' '.join(words)
+                spans.append(span)
+        return spans
 
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pandas as pd
 
 from dataset import *
@@ -13,7 +15,7 @@ def test_idx():
 
 def test_essays():
     dataset = ArgumentDataset()
-    essays = dataset.essays()
+    essays = dataset.essays(10)
     essay = next(essays)
     assert(isinstance(essay, pd.DataFrame))
     assert(len(essay) == 10)
@@ -22,6 +24,30 @@ def test_essays():
     next_essay = next(essays)
     next_ids = list(next_essay.loc[:,'id'])
     assert(ids[0] != next_ids[0])
-    essay_count = sum(1 for _ in dataset.essays())
-    assert(essay_count == 15594)
+    essay_count = sum(1 for _ in dataset.essays(10))
+    assert(essay_count == 10)
+
+def test_arguments():
+    dataset = ArgumentDataset()
+    arguments = dataset.arguments(20)
+    assert(len(list(arguments)) == 20)
+
+def test_essay_paths():
+    dataset = ArgumentDataset()
+    paths = dataset.essay_paths()
+    assert(isinstance(paths[0], Path))
+    assert(len(paths) == 15594)
+
+def test_random_essay():
+    dataset = ArgumentDataset()
+    random_essay = dataset.random_essay(num_essays=5)
+    assert(len(random_essay) == 5)
+    assert(isinstance(random_essay[0], str))
+
+def test_random_span():
+    dataset = ArgumentDataset()
+    random_span = dataset.random_span(num_words=5, num_spans=10)
+    assert(len(random_span) == 10)
+    assert(isinstance(random_span[0], str))
+    assert(len(random_span[0].split()) == 5)
 
