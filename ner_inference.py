beb97b003f3489eaf48c431724fcf9ba8b21b98f
Edmund Mills <edmund.martin.mills@gmail.com>
2022-01-24 09:26:41 -0800

    Update NER inference script to use folds

--- ner_inference.py
+++ ner_inference.py
@@ -1,11 +1,10 @@
 import os
 
 import numpy as np
-import pickle
+from pathlib import Path
 import random
 import transformers
 import torch
-import wandb
 
 random.seed(0)
 np.random.seed(0)
@@ -22,23 +21,25 @@ if __name__ == '__main__':
     args = parse_args()
     args = get_config('base', args)
 
-    if args.wandb:
-        wandb.tensorboard.patch(root_logdir="log")
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     transformers.logging.set_verbosity_error()
     
-    if args.debug:
-        dataset = EssayDataset(n_essays=3)
-    else:
-        dataset = EssayDataset()
+    dataset = EssayDataset.load(args.dataset_path)
+    
+    tokenizer = NERTokenizer(args.ner)
+    
+    for fold in dataset.folds:
+        print(f'Starting inference on fold {fold}')
+        _train, val = dataset.get_fold(fold)
 
+        ner_model = NERModel(args.ner)
+        ner_model_name = f'{args.ner_model_name}_fold_{fold}'
+        model_dir = Path(f'./models/{ner_model.__class__.__name__}')
+        model_file = next(model_dir.glob(f'*{ner_model_name}*')).stem
+        ner_model.load(model_file)
 
-    ner_tokenizer = NERTokenizer(args.ner)
-    ner_model = NERModel(args.ner)
-    ner_model.load(args.ner.ner_model_name)
-    dataset.ner_probs = ner_model.infer_for_dataset(dataset, ner_tokenizer)
-    print(f'Saving NER Probs to {ner_probs_path}')
-    with open(ner_probs_path, 'wb') as saved_file:
-        pickle.dump(dataset.ner_probs, saved_file)
-    print('NER probs saved')
+        ner_probs = ner_model.infer_for_dataset(val, tokenizer)
+        dataset.ner_probs.update(ner_probs)
+
+    dataset.save(args.ner_dataset_path)
 
