806ec19785cc678fe332e1e865567e25f6672f0d
Edmund Mills <edmund.martin.mills@gmail.com>
2022-01-24 09:32:27 -0800

    Update predict_by_heuristics.py

--- predict_by_heuristics.py
+++ predict_by_heuristics.py
@@ -1,11 +1,6 @@
-import os
-
 import numpy as np
-import pickle
 import random
-import transformers
 import torch
-import wandb
 
 random.seed(0)
 np.random.seed(0)
@@ -13,11 +8,8 @@ torch.manual_seed(0)
 torch.cuda.manual_seed_all(0)
 
 from core.dataset import EssayDataset
-from core.env import SegmentationEnv
-from core.essay import Prediction
 from core.predicter import Predicter
 from utils.config import parse_args, get_config
-from utils.postprocessing import link_evidence, proba_thresh, min_thresh
 from utils.render import plot_ner_output
 
 
@@ -25,25 +17,16 @@ if __name__ == '__main__':
     args = parse_args()
     args = get_config('base', args)
 
-    if args.wandb:
-        wandb.tensorboard.patch(root_logdir="log")
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-    transformers.logging.set_verbosity_error()
-    
-    dataset = EssayDataset(100)
-
-
-    ner_probs_path = 'data/ner_probs.pkl'
-    with open(ner_probs_path, 'rb') as saved_file:
-        dataset.ner_probs = pickle.load(saved_file)
-    print(f'NER Probs Loaded from {ner_probs_path}')
+    dataset = EssayDataset.load(args.ner_dataset_path)
 
     predicter = Predicter()
 
     scores = []
+    count = 100
     for essay in dataset:
         _preds, metrics = predicter.by_heuristics(essay)
         score = metrics['f_score']
         scores.append(score)
+        if count and len(scores > count): break
     avg_score = sum(scores) / len(scores)
     print(avg_score)
